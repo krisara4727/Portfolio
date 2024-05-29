1a7db67b1e6d673e3c54a9988e9cb0e3aaa36b00
krishna.chivte <krishna.chivte@in.abb.com>
2024-05-30 00:24:28 +0530

    backend url added.

--- src/frontend/src/api/axios.ts
+++ src/frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.NODE_ENV !== "development"? "https://portfolio-backend-liard-chi.vercel.app/api":'http://localhost:5000/api',
   timeout: 10000,
 });
 
